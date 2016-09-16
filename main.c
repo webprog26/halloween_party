359dc068c879468da39cd8b5a53873af2fe2fd70
webprog26 <webprog26@gmail.com>
2016-09-16 09:28:27 +0300

    Testing version 1

--- main.c
+++ main.c
@@ -12,8 +12,27 @@ int getHorizontalCuts(int attempts)
     return attempts / 2 + (attempts % (attempts / 2));
 }
 
+int calculateResult(int attempts)
+{
+    int horizontalCuts = getHorizontalCuts(attempts);
+    int verticalCuts = attempts - horizontalCuts;
+
+    return horizontalCuts * verticalCuts;
+}
+
 int main()
 {
+    /*
+    int t;
+    int k;
 
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++)
+    {
+        scanf("%d", &k);
+        printf("%i\n", calculateResult(k));
+    }
+    */
     return 0;
 }

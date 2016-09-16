b0defc5fac9c6d69346e3064ab297d88396869bf
webprog26 <webprog26@gmail.com>
2016-09-16 09:15:44 +0300

    Added method int getGorizontalCuts(int attempts)

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int getHorizontalCuts(int attempts)
+{
+    if(attempts % 2 == 0)
+    {
+        return attempts / 2;
+    }
+
+    return attempts / 2 + (attempts % (attempts / 2));
+}
+
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
